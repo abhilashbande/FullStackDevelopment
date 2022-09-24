a4db2232792ab474739cad8f033a579b12fefa5b
abhilash.bande <abhilash.bande@gmail.com>
2022-09-24 12:40:12 +0530

    Session 24 Sept - JavaScript Events

---
+++ scripts/1.js
@@ -0,0 +1,22 @@
+function submitClick() {
+    console.log("submitClick");
+    // alert("clicked");
+    // confirm("Are you sure?");
+    // open("./../html/Assignment_4/login.html", "loginPage");
+    open("./../html/Assignment_4/login.html", "loginPage", "width=500px", "height=width=800px", "height=width=800px");
+}
+
+function submitOver() {
+    console.log("onMouseOver");
+    // alert("Submit Over");
+}
+
+function submitOut() {
+    console.log("onMouseOut");
+    // alert("Submit Out"); 
+}
+
+// load an image on button click event
+// open and close a window using 2 different buttons (open close button)
+// open and close a window using same
+// open a window and it should be moving 
