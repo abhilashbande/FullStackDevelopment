a934aa8d6a4d7345ce9b395ea6548f520a977bd3
abhilash.bande <abhilash.bande@gmail.com>
2022-10-02 12:29:31 +0530

    Session02Oct - JS Strings

---
+++ scripts/5_Strings.js
@@ -0,0 +1,17 @@
+var str = "India is my country";
+console.log(str);
+console.log("Length is : " + str.length);
+console.log("Index position of 'i' is : " + str.indexOf("i"));
+console.log("Index position of 'nd' is : " + str.indexOf("nd"));
+console.log(
+    "Index position of 'a' is starting from a pos: " + str.indexOf("i", 4)
+);
+console.log("Char at 2 is : " + str.charAt(2));
+console.log("Substr 0 to next 9 char : " + str.substring(4, 10));
+console.log("To UpparCase : " + str.toUpperCase());
+
+// Assignments
+// 1. create a countdown timer using date function
+// 2. count total number of occurrences of a given character in given string
+// 3. check if the string is palindrome or not
+// 4. Find (list/display) duplicate characters in given string
